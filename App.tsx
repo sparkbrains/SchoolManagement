d52add978ff175d1fa2edf5d39a53a60dc3cf9b6
nikhil-mahto <nikhil@sparkbrains.in>
2025-05-02 12:06:26 +0530

    worked on sidebar

--- App.tsx
+++ App.tsx
@@ -1,16 +1,50 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import React from 'react';
+import {StatusBar} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import ForgotPasswordScreen from './src/screens/ForgotPassword';
-import {RootStackParamList} from './src/types/screen-props';
 import CameraView from './src/screens/CameraView';
 import {AuthProvider, useAuth} from './src/components/context/AuthContext';
-import {StatusBar} from 'react-native';
 import {AppProvider} from './src/components/context/AppContext';
+import {RootStackParamList} from './src/types/screen-props';
+import 'react-native-reanimated';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const AuthStack = createNativeStackNavigator<RootStackParamList>();
+const Drawer = createDrawerNavigator<RootStackParamList>();
+
+function AuthNavigator(): React.JSX.Element {
+  return (
+    <AuthStack.Navigator initialRouteName="Login">
+      <AuthStack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <AuthStack.Screen
+        name="ForgotPassword"
+        component={ForgotPasswordScreen}
+        options={{headerShown: false}}
+      />
+    </AuthStack.Navigator>
+  );
+}
+
+function DrawerNavigator(): React.JSX.Element {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen
+        name="CameraView"
+        component={CameraView}
+        options={{headerShown: false}}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 function App(): React.JSX.Element {
   const {isLoggedIn, isLoading} = useAuth();
@@ -22,34 +56,19 @@ function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
-      <Stack.Navigator initialRouteName={isLoggedIn ? 'Home' : 'Login'}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="ForgotPassword"
-          component={ForgotPasswordScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="CameraView"
-          component={CameraView}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
+      {isLoggedIn ? <DrawerNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   );
 }
 
 export default function AppWrapper(): React.JSX.Element {
   return (
-    <AuthProvider>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </AuthProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <AuthProvider>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
